9042affe8577e6f6b7accbd81b5d745807e4a7b6
Shane Maloney <shane.maloney@dias.ie>
2026-09-03 21:23:51 +0100

    Fix tests

--- smart/tests/test_calculate_properties.py
+++ smart/tests/test_calculate_properties.py
@@ -5,6 +5,6 @@ from smart.calculate_properties import smart_indentify_and_characterize
 
 def test_smart():
     cur_hmi_map = Map("http://jsoc.stanford.edu/data/hmi/fits/2024/06/06/hmi.M_720s.20240606_000000_TAI.fits")
-    prev_hmi_map = Map("http://jsoc.stanford.edu/data/hmi/fits/2024/06/05/hmi.M_720s.20240605_000000_TAI.fits")
+    prev_hmi_map = Map("http://jsoc1.stanford.edu/data/hmi/fits/2024/06/05/hmi.M_720s.20240605_010000_TAI.fits")
     properties = smart_indentify_and_characterize(cur_hmi_map, prev_hmi_map)
     assert len(properties) > 0
